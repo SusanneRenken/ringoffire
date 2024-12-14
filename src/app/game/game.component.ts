e7a82bb5c8a9a6dff9f49a035e3260285a293916
SusanneRenken <renken.susanne@gmail.com>
2024-12-14 22:08:59 +0100

    turn card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+
+export class GameComponent implements OnInit{
   pickCardAnimation = false;
+  game?: Game; //Das Fragezeichen ist von mir - Bei Junus geht es ohne, warum auch immer
+
+  newGame(){
+    this.game = new Game();
+  }
+
+  ngOnInit(): void{
+    this.newGame();
+    console.log(this.game);
+    
+  }
 
   takeCard(){
     this.pickCardAnimation = true;
