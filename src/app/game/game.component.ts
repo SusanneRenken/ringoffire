7639138e1e35b03d2e20f0832bb4e7ed72e1cefe
SusanneRenken <renken.susanne@gmail.com>
2024-12-21 20:11:54 +0100

    active player and description field

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 @Component({
   selector: 'app-game',
@@ -10,6 +12,10 @@ export class GameComponent implements OnInit {
   pickCardAnimation = false;
   currentCard: string = '';
   game?: Game; //Das Fragezeichen ist von ChatGPT - Bei Junus geht es ohne, warum auch immer
+
+  constructor(public dialog: MatDialog){
+
+  }
 
   ngOnInit(): void {
     this.newGame();
@@ -24,14 +30,24 @@ export class GameComponent implements OnInit {
     if (this.game && this.game.stack.length > 0) {
       this.currentCard = this.game.stack.pop() as string;
 
-      // this.pickCardAnimation = true;
-      this.game.playedCards.push(this.currentCard);
+      this.pickCardAnimation = true;
 
-      // setTimeout(() => {
-      //   this.pickCardAnimation = false;
-      // }, 1000);
+      this.game.currentPlayer = (this.game.currentPlayer + 1) % this.game.players.length;
+      
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+        this.game?.playedCards.push(this.currentCard);
+      }, 1000);
     }
   }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe(name => {
+      console.log(name);
+      this.game?.players.push(name);
+    });
+  }
 
 }
